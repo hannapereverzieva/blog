8b1bd198c6eed718d8a4851264dbb7de8c402962
Hanna Pereverzieva <hannapereverzieva@intellias.com>
2021-12-30 03:06:10 +0200

    Connected MongoDB

--- backend/app.js
+++ backend/app.js
@@ -26,6 +26,31 @@ app.use((req, res, next) => {
     next();
 });
 
+//mocked data
+const posts = [
+    {
+        id: "123",
+        date: "June 4th",
+        title: "Shiba Inu 1",
+        author: "John Galt",
+        content: "The Shiba Inu is the smallest of the six original and distinct spitz."
+    },
+    {
+        id: "123",
+        date: "July 15",
+        title: "Shiba Inu 2",
+        author: "John Galt",
+        content: "The Shiba Inu is the smallest of the six original and distinct spitz."
+    },
+    {
+        id: "123",
+        date: "May 5th",
+        title: "Shiba Inu 3",
+        author: "John Galt",
+        content:"The Shiba Inu is the smallest of the six original and distinct spitz."
+    }
+]
+
 app.post("/api/posts", (req, res, next)=> {
     const post = new Post({
         title: req.body.title,
@@ -33,7 +58,6 @@ app.post("/api/posts", (req, res, next)=> {
         author: req.body.author,
         date: req.body.date
     })
-    console.log(post);
     post.save();
     res.status(201).json({
         message: 'Post was posted successfully!'
@@ -41,55 +65,38 @@ app.post("/api/posts", (req, res, next)=> {
 });
 
 app.get("/api/posts", (req, res,next) => {
+
+    //aggregate practice
+    Post.aggregate([
+        {$limit: 25},
+        {$match: {author: "Hanna"}},
+    ])
+
+    //sort practice
     Post.find()
+        .sort({"date": -1})
         .then(documents => {
             res.status(200).json({
                 message: "Posts were fetched successfully!",
                 posts: documents
             });
         })
-    const posts = [
-        {
-            id: "123",
-            date: "June 4th",
-            title: "Shiba Inu 1",
-            author: "John Galt",
-            content: "The Shiba Inu is the smallest of the six original and distinct spitz."
-        },
-        {
-            id: "123",
-            date: "July 15",
-            title: "Shiba Inu 2",
-            author: "John Galt",
-            content: "The Shiba Inu is the smallest of the six original and distinct spitz."
-        },
-        {
-            id: "123",
-            date: "May 5th",
-            title: "Shiba Inu 3",
-            author: "John Galt",
-            content:"The Shiba Inu is the smallest of the six original and distinct spitz."
-        }
-    ]
-
-
-
-    app.get("*", (req, res, next) => {
-        res.status(404).json({
-            message: "Oops, this is not a valid URL."
-        })
-    });
-
-    app.use((err, req, res, next) => {
-        console.error(err);
-        res.status(500).json({
-            status: 500,
-            message: 'Internal server error',
-            type: 'internal'
-        });
-    })
-
 });
+
+app.use("*", (req, res, next) => {
+    res.status(404).json({
+        message: "Oops, this is not a valid URL."
+    })
+});
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).json({
+        status: 500,
+        message: 'Internal server error',
+        type: 'internal'
+    });
+})
 
 app.use(express.static("dist/blog"));
 module.exports = app;
